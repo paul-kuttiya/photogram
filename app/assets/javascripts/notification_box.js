838cbe924f01102e2215d54bc3ab1f80fd8646f0
pookzzz2231 <w.kuttiya@gmail.com>
2017-08-21 02:04:26 -0400

    add notice box

---
+++ app/assets/javascripts/notification_box.js
@@ -0,0 +1,22 @@
+document.addEventListener("turbolinks:load", function()
+{
+$("#notificationLink").click(function()
+{
+$("#notificationContainer").fadeToggle(300);
+$("#notification_count").fadeOut("slow");
+return false;
+});
+
+//Document Click hiding the popup 
+$(document).click(function()
+{
+$("#notificationContainer").hide();
+});
+
+//Popup on click
+$("#notificationContainer").click(function()
+{
+return false;
+});
+
+});

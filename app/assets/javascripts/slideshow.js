b6028c2b18a0f60145b7d8bcb904a79880d08207
pookzzz2231 <w.kuttiya@gmail.com>
2017-08-25 00:01:47 -0400

    Add slideshow

---
+++ app/assets/javascripts/slideshow.js
@@ -0,0 +1,29 @@
+document.addEventListener("turbolinks:load", function() {
+  $slide = $('.user-form-wrapper .slideshow .slide-photo');
+
+  var slide_time = 1500,
+      i = 2;
+  
+    setInterval(function() {
+      $slide.animate({
+        opacity: 0,
+      }, slide_time, function() {
+        $slide.css('background-image', `url(/placeholder/slide_${i}.jpeg)`).animate({
+          opacity: 1
+        }, slide_time, function() {
+          i++;
+        });
+      });
+
+      i = i >= 5 ? 0 : i
+    }, 3000);
+
+  // setInterval(function() { 
+  // $slide.css('background', )
+  //   .fadeOut(1000)
+  //   .next()
+  //   .fadeIn(1000)
+  //   .end()
+  //   .appendTo('#slideshow');
+  // },  3000);
+});
